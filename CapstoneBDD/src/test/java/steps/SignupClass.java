47c3ce275ca1114279c75c00e30f8cefd157bc35
atgore <atgore@192.168.0.4>
2021-12-24 23:13:26 +0530

    first commit

---
+++ CapstoneBDD/src/test/java/steps/SignupClass.java
@@ -0,0 +1,32 @@
+package steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignupClass {
+	WebDriver driver;
+
+	public SignupClass(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+	}
+
+	public void SignUp() {
+
+		driver.findElement(By.xpath("//a[text()='Login/Signup']")).click();
+		System.out.println("user has opened login page");
+		driver.findElement(By.xpath("//a[text()='Not a member? Signup']")).click();
+		System.out.println("user opts to register");
+		driver.findElement(By.name("email_id")).sendKeys("atgore@domain.com");
+		driver.findElement(By.name("pwd")).sendKeys("atgore");
+		driver.findElement(By.name("pwd2")).sendKeys("atgore");
+		driver.findElement(By.name("name")).sendKeys("atgore");
+		System.out.println("user enters details required");
+		driver.findElement(By.xpath("//button[text()='Signup']")).click();
+		driver.findElement(By.xpath("//a[text()='Already a member? Login']")).click();
+		System.out.println("user registered successfully");
+
+
+	}
+}
+
